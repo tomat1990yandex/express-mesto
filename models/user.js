0c46af1baa718cb27e12c0e920d8970c89359c26
Dmitriy Orlov <tomat1990@yandex.ru>
2021-10-29 13:25:08 +0500

    Project 14

--- models/user.js
+++ models/user.js
@@ -46,8 +46,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+userSchema.statics.findUserByCredentials = function validate(email, password) {
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Не корректный пароль или email'));
