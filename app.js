2a89a10ee30500ccdc413ae5aa363f198853c1f9
Dmitriy Orlov <tomat1990@yandex.ru>
2021-10-30 14:46:09 +0500

    Project 14 validation fix

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.post('/signup',
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
     }),
   }),
   createUser);
